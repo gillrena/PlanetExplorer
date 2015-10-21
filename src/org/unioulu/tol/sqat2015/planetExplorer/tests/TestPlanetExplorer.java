7a80f0dc60da310f633a56f01a4013daa6f3d099
somename <someemail>
2015-10-21 16:09:33 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqat2015/planetExplorer/tests/TestPlanetExplorer.java
+++ src/org/unioulu/tol/sqat2015/planetExplorer/tests/TestPlanetExplorer.java
@@ -9,8 +9,8 @@ public class TestPlanetExplorer {
 
 	@Test
 	public void testPlanetLanding() {
-		PlanetExplorer planetExplorer = new PlanetExplorer(100, 100, null, null);
-		String expectResult = planetExplorer.executeCommand(null);
+		PlanetExplorer planetExplorer = new PlanetExplorer(100, 100, "", "");
+		String expectResult = planetExplorer.executeCommand("");
 		System.out.println(expectResult);
 		assertEquals("(0,0,N)", expectResult);
 	}
