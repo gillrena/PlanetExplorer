cd1ce7e347d606fba04494e2abd09d7719a95647
somename <someemail>
2015-10-21 16:04:06 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqat2015/planetExplorer/tests/TestPlanetExplorer.java
+++ src/org/unioulu/tol/sqat2015/planetExplorer/tests/TestPlanetExplorer.java
@@ -10,7 +10,7 @@ public class TestPlanetExplorer {
 	@Test
 	public void testPlanetLanding() {
 		PlanetExplorer planetExplorer = new PlanetExplorer(100, 100, "", "");
-		String expectResult = planetExplorer.executeCommand("");
+		String expectResult = planetExplorer.executeCommand("f");
 		System.out.println(expectResult);
 	}
 }
